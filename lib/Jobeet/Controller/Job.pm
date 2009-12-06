dae952157900601948c612c8bd16d4d09dda6ed9
Daisuke Murase <typester@cpan.org>
2009-12-06 23:00:02 +0900

    day 06

--- lib/Jobeet/Controller/Job.pm
+++ lib/Jobeet/Controller/Job.pm
@@ -9,5 +9,28 @@ sub index :Path {
     $c->stash->{jobs} = models('Schema::Job');
 }
 
+sub show :Path :Args(1) {
+    my ($self, $c, $job_id) = @_;
+}
+
+sub create :Local {
+    my ($self, $c) = @_;
+}
+
+sub job :Chained('/') :PathPart :CaptureArgs(1) {
+    my ($self, $c, $job_id) = @_;
+    $c->stash->{job_id} = $job_id;
+}
+
+sub edit :Chained('job') :PathPart {
+    my ($self, $c) = @_;
+}
+
+# /job/{job_id}/delete （削除）
+sub delete :Chained('job') :PathPart {
+    my ($self, $c) = @_;
+}
+
 1;
+
 
